6e5f82353a36b4b5690037253fdc8dde5271e16c
Chris <631963997@qq.com>
2022-05-31 13:10:52 +0800

    feat:resource-number

---
+++ src/resource-number/total-small-100.js
@@ -0,0 +1,301 @@
+// 100个small-js合并
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
---
+++ src/resource-number/total-small-25.js
@@ -0,0 +1,77 @@
+// 100个small-js合并
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+(function () {
+  console.log('small-js');
+})();
+
