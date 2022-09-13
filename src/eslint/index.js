35485ade9f4061f81665eb78a0d908eac2a57088
zhanghe <631963997@qq.com>
2022-09-13 18:39:07 +0800

    husky-demo3

---
+++ src/eslint/index.js
@@ -0,0 +1,16 @@
+function printUser(firstName, lastName, number, street, code, city, country) {
+  console.log(
+    `${firstName} ${lastName} lives at ${number}, ${street}, ${code} in ${city}, ${country}`
+  );
+}
+printUser(
+  "John",
+  "Doe",
+  48,
+  "998 Primrose Lane",
+  53718,
+  "Madison",
+  "United States of America"
+);
+var a=1;
+console.log(a);
