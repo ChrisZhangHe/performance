62ca727e26bc5e1f96911a35009a0a3224c56e8b
Chris <631963997@qq.com>
2022-09-14 11:31:51 +0800

    husky-demo17

--- src/eslint/webpack.config.js
+++ src/eslint/webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    },
-    plugins: [new ESLintPlugin()],
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  plugins: [new ESLintPlugin()],
 };
