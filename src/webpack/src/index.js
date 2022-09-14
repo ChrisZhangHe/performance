4fc7846d347875c00fdef949b8e0348052b52633
Chris <631963997@qq.com>
2022-09-14 18:29:18 +0800

    refactor: 修改工程名称为webpack;同步修改husky配置;

---
+++ src/webpack/src/index.js
@@ -0,0 +1,23 @@
+function printUser(firstName, lastName, number, street, code, city, country) {
+  console.log(
+    `${firstName} ${lastName} lives at ${number}, ${street}, ${code} in ${city}, ${country}`
+  );
+}
+printUser(
+  "John",
+  "Doe",
+  48,
+  "998 Primrose Lane",
+  53718,
+  "Madison",
+  "United States of America"
+);
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+console.log(a, b, c, d, e, dd);
+var dd = 5;
+var test;
+console.log(test);
