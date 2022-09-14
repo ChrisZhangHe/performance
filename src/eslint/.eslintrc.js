6d94bd989d100ed7a49349a82d1cfae391d994bc
Chris <631963997@qq.com>
2022-09-14 11:21:03 +0800

    husky-demo15

--- src/eslint/.eslintrc.js
+++ src/eslint/.eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node":true
-    },
-    "extends": ["eslint:recommended","prettier"],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": ["prettier"],
-    "rules": {
-      "prettier/prettier": "error"
-    }
-}
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
