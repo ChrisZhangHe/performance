cc861cd8ed9f14f9f2b20d11608d7592d75e1e32
Chris <631963997@qq.com>
2022-09-15 07:59:42 +0800

    feat:add webpack-dev-serve elint-plugin

--- src/webpack/webpack.config.js
+++ src/webpack/webpack.config.js
@@ -1,11 +1,38 @@
 const path = require("path");
 const ESLintPlugin = require("eslint-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index_bundle.js",
   },
-  plugins: [new ESLintPlugin()],
+  mode: "development", // process.env.NODE_ENV,
+  plugins: [
+    // html打包插件
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ESLintPlugin({}),
+  ],
+  devServer: {
+    static: {
+      // 静态资源默认路径
+      directory: path.join(__dirname, "dist"),
+    },
+    // compress: true, // 启用gzip压缩
+    port: 8080, // 端口
+    // open: true, // 自动打开服务
+    hot: true, // 热加载
+    // devMiddleware: {
+    //   // 用于检测文件更改自动打包，并刷新页面
+    //   index: true,
+    //   mimeTypes: { phtml: "text/html" },
+    //   publicPath: "/",
+    //   serverSideRender: true,
+    //   writeToDisk: true,
+    // },
+  },
+  target: "web", // 目标是浏览器
 };
